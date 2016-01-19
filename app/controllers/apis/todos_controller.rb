bfeecaf96c44a1c1d451d2e3e577cc46ac94d6b3
App Academy Student <appacademy@Apps-Mac-mini-101.local>
2016-01-19 12:20:08 -0500

    Create TodosController.'

---
+++ app/controllers/apis/todos_controller.rb
@@ -0,0 +1,42 @@
+class Api::TodosController < ApplicationController
+  def index
+    render json: Todo.all.to_json
+    # render json: Recipe.all.includes(:ingredients).to_json(include: :ingredients)
+  end
+
+  def show
+    todo = Todo.find(params[:id])
+    render json: todo.to_json
+  end
+
+  def create!
+    todo = Todo.new(todo_params)
+    todo.save!
+    render json: todo.to_json
+
+    # if todo.save
+    #   render json: todo
+    # else
+    #   render json: { errors: todo.errors.full_messages }, status: 422
+    # end
+  end
+
+  def destroy!
+    todo = Todo.find(params[:id])
+    todo.destroy
+    # render json: something
+  end
+
+  def update!
+    todo = Todo.find(params[:id])
+    todo.update(todo_params)
+    todo.save!
+    render json: todo.to_json
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :body)
+  end
+end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -11,4 +11,7 @@
 #
 
 class Todo < ActiveRecord::Base
+  validates :title, :body, presence: true
+  validates_inclusion_of :done, in: [true, false]
+
 end
